26c895af1f1197cc58c1bfac6ad0b9677352dc94
tcdev1545347 <112838818+tcdev1545347@users.noreply.github.com>
2022-12-12 13:24:38 -0800

    Update hash_table.cpp

--- hash_table.cpp
+++ hash_table.cpp
@@ -31,7 +31,8 @@ int main()
     insert(20);
     insert(30);
     insert(40);
-    insert(3);
+    //Uncomment for Collision Case
+    //insert(3)
     for (auto x : table) {
         std::cout << x << std::endl;
     }
